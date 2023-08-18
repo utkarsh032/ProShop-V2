15ab0019b37020af471744a83343acdce26e6f53
utkarsh032 <utkarshhraj525@gmail.com>
2023-08-19 00:12:37 +0530

    ProShop Setup

---
+++ backend/models/productModel.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  reviews: [reviewsSchema],
+  rating: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  numReviews: {
+    type: Number,
+    required: true,
+    default: 0,
+  },price:{
+    
+  }
+});
