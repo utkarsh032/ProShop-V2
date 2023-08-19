01ca26d1793b23a2562b7815b2c250e47b13f184
utkarsh032 <utkarshhraj525@gmail.com>
2023-08-19 15:18:46 +0530

    Prepare Sample Data

---
+++ backend/data/users.js
@@ -0,0 +1,25 @@
+import bcrypt from "bcryptjs";
+
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@email.com",
+    password: "bcrypt.hashSync('123456',10)",
+    isAdmin: true,
+  },
+
+  {
+    name: "Utkarsh Raj",
+    email: "utkarsh@email.com",
+    password: "bcrypt.hashSync('123456',10)",
+    isAdmin: true,
+  },
+  {
+    name: "Jal Raj",
+    email: "jal@email.com",
+    password: "bcrypt.hashSync('123456',10)",
+    isAdmin: true,
+  },
+];
+
+export default users;
