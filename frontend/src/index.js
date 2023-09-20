ffd7ccaa8c8badd3c2698d9975d92e2de80c436c
utkarsh032 <utkarshhraj525@gmail.com>
2023-09-20 22:00:39 +0530

    Shipping Screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,6 +17,7 @@ import LoginScreen from "./screens/LoginScreen";
 import store from "./store";
 import { Provider } from "react-redux";
 import RegisterScreen from "./screens/RegisterScreen";
+import ShippingScreen from "./screens/ShippingScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -26,6 +27,7 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
+      <Route path="/shipping" element={<ShippingScreen />} />
     </Route>
   )
 );
